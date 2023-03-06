26275f6df0df1c0a6eb9b5c6cbfe08d773e3df9e
swetagh <swetaghose16@gmail.com>
2023-03-05 20:11:05 -0600

    cleaned up code

--- p4/src/pages/Home.js
+++ p4/src/pages/Home.js
@@ -9,24 +9,6 @@ import { auth } from '../firebase';
 const Home = () => {
     const navigate = useNavigate();
 
-    // useEffect(()=>{
-    //     onAuthStateChanged(auth, (user) => {
-    //         if (user) {
-    //           // User is signed in, see docs for a list of available properties
-    //           // https://firebase.google.com/docs/reference/js/firebase.User
-    //           const uid = user.uid;
-    //           // ...
-    //           console.log("uid", uid)
-    //         } else {
-    //           // User is signed out
-    //           // ...
-    //           console.log("user is logged out")
-    //         }
-    //       });
-         
-    // }, [])
-
-
     const handleSignUp = () => {               
         navigate("/signup");
     }
